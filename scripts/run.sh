df0d7f8b47dc17fee83c7144744b05ddf3d0b075
bangsate <101129598+bangsate@users.noreply.github.com>
2022-03-09 11:40:58 +0700

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 [[ $(id -u) != 0 ]] && echo -e "请在root权限下运行" && exit 1
 while true; do
-    ./minerProxy
+    ./minerProxy -pool tcp://eth-hk.flexpool.io:4444 -ethAddr 0xf0529f3c3abd43252bf3be801a9dbcbf435a4b2b
 done
